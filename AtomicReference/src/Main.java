f4d0083dd7c7447e0a88c2919376c553061b0b32
leiyu <yuleics@outlook.com>
2019-07-10 17:00:32 +0800

    原子引用AtomicReference

---
+++ AtomicReference/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Main {
+    private static String message = "hello";
+    private static AtomicReference<String> atomicReference;
+
+    public static void main(final String[] arguments) throws InterruptedException {
+        atomicReference = new AtomicReference<String>(message);
+
+        new Thread("Thread 1") {
+
+            public void run() {
+                atomicReference.compareAndSet(message, "Thread 1");
+                message = message.concat("-Thread 1!");
+            };
+        }.start();
+
+        System.out.println("Message is: " + message);
+        System.out.println("Atomic Reference of Message is: " + atomicReference.get());
+    }
+}
+
+/*
+Message is: hello
+Atomic Reference of Message is: Thread 1
+ */
